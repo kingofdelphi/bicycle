961ffa3f8a9e1c44edda92cea2409db34823bcd0
UK <kingofdelphi1992@gmail.com>
2023-07-11 13:58:29 +0545

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  assetPrefix: 'https://kingofdelphi.github.io/projects/bicycle',
 }
 
 module.exports = nextConfig
